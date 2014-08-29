59eedf9327b1dfaa64bcd0bce5e88e447e8a1454
Chris Hourihan <chourihan@procur.com>
2014-08-29 18:10:57 -0400

    customizable colors of snackbar

--- assets/javascripts/snackbar_module.js
+++ assets/javascripts/snackbar_module.js
@@ -13,11 +13,6 @@
       BOTTOM_RIGHT: 'BOTTOM_RIGHT',
       TOP_RIGHT: 'TOP_RIGHT',
       BOTTOM_LEFT: 'BOTTOM_LEFT'
-    })
-    .constant('SNACKBAR_COLORS', {
-      SUCCESS: '#5cb85c',
-      ERROR: '#d9534f',
-      DEFAULT: '#333'
     });
 
 })(angular);
--- dist/chSnackbar.js
+++ dist/chSnackbar.js
@@ -26,11 +26,6 @@ angular.module('ch.Snackbar.Templates', []).run(['$templateCache', function($tem
       BOTTOM_RIGHT: 'BOTTOM_RIGHT',
       TOP_RIGHT: 'TOP_RIGHT',
       BOTTOM_LEFT: 'BOTTOM_LEFT'
-    })
-    .constant('SNACKBAR_COLORS', {
-      SUCCESS: '#5cb85c',
-      ERROR: '#d9534f',
-      DEFAULT: '#333'
     });
 
 })(angular);
@@ -38,150 +33,171 @@ angular.module('ch.Snackbar.Templates', []).run(['$templateCache', function($tem
 // assets/javascripts/snackbar.js
 (function(angular) {
 
-  var
-    definitions;
-
-  definitions = [
-    '$document',
-    '$rootScope',
-    '$templateCache',
-    '$compile',
-    '$timeout',
-    '$animate',
-    'POSITIONS',
-    'SNACKBAR_COLORS',
-    snackbarService
-  ];
-
   angular.module('ch.Snackbar')
-    .factory('snackbar', definitions);
+    .provider('snackbar', snackbarProvider);
 
-  function snackbarService($document, $rootScope, $templateCache, $compile, $timeout, $animate, POSITIONS, COLORS) {
+  function snackbarProvider() {
     var
-      templateUrl = 'snackbar.html',
-      template = $templateCache.get(templateUrl),
-      scope = $rootScope.$new(),
-      body = $document.find('body'),
-      POP_UP = 'snackbar-pop-up',
-      POP_OUT = 'snackbar-pop-out',
-      POP_OUT_TIMEOUT = 4000,
-      REMOVE_TIMEOUT = 4200,
-      POSITION_CLASSES,
-      stack = [];
+      colors,
+      definitions;
 
-    POSITION_CLASSES = {
-      TOP_LEFT: 'snackbar-top-left',
-      BOTTOM_RIGHT: 'snackbar-bottom-right',
-      TOP_RIGHT: 'snackbar-top-right',
-      BOTTOM_LEFT: 'snackbar-bottom-left'
+    colors = {
+      success: '#5cb85c',
+      error: '#d9534f',
+      notice: '#333'
     };
+
+    definitions = [
+      '$document',
+      '$rootScope',
+      '$templateCache',
+      '$compile',
+      '$timeout',
+      '$animate',
+      'POSITIONS',
+      snackbarFactory
+    ];
 
     return {
-      success: success,
-      error: error,
-      notice: notice
+      setColors: setColors,
+      $get: snackbarFactory
     };
 
-    function success(message, pos) {
-      var
-        successConfig;
-
-      successConfig = {
-        'background-color': COLORS.SUCCESS
-      };
-
-      notice(message, pos, successConfig);
+    function setColors(config) {
+      colors.success = config.success || colors.success;
+      colors.error = config.error || colors.error;
+      colors.notice = config.notice || colors.notice;
     }
 
-    function error(message, pos) {
+    function snackbarFactory($document, $rootScope, $templateCache, $compile, $timeout, $animate, POSITIONS) {
       var
-        errorConfig;
+        templateUrl = 'snackbar.html',
+        template = $templateCache.get(templateUrl),
+        scope = $rootScope.$new(),
+        body = $document.find('body'),
+        POP_UP = 'snackbar-pop-up',
+        POP_OUT = 'snackbar-pop-out',
+        POP_OUT_TIMEOUT = 4000,
+        REMOVE_TIMEOUT = 4200,
+        POSITION_CLASSES,
+        stack = [];
 
-      errorConfig = {
-        'background-color': COLORS.ERROR
+      POSITION_CLASSES = {
+        TOP_LEFT: 'snackbar-top-left',
+        BOTTOM_RIGHT: 'snackbar-bottom-right',
+        TOP_RIGHT: 'snackbar-top-right',
+        BOTTOM_LEFT: 'snackbar-bottom-left'
       };
 
-      notice(message, pos, errorConfig);
-    }
+      return {
+        success: success,
+        error: error,
+        notice: notice
+      };
 
-    function notice(message, pos, config) {
-      var
-        snackbar,
-        styles,
-        position;
+      function success(message, pos) {
+        var
+          successConfig;
 
-      config = config || {};
+        successConfig = {
+          'background-color': colors.success
+        };
 
-      if (message) {
-        styles = getStyles();
-        position = getPosition();
+        notice(message, pos, successConfig);
+      }
 
-        scope.message = message;
-        scope.styles = styles;
-        scope.position = position;
+      function error(message, pos) {
+        var
+          errorConfig;
 
-        snackbar = $compile(template)(scope);
+        errorConfig = {
+          'background-color': colors.error
+        };
 
-        if (stack.length) {
-          for(var i = 0, len = stack.length; i < len; i++) {
-            clearSnackbar(stack[i], i);
+        notice(message, pos, errorConfig);
+      }
+
+      function notice(message, pos, config) {
+        var
+          snackbar,
+          styles,
+          position;
+
+        config = config || {};
+
+        if (message) {
+          styles = getStyles();
+          position = getPosition();
+
+          scope.message = message;
+          scope.styles = styles;
+          scope.position = position;
+
+          snackbar = $compile(template)(scope);
+
+          if (stack.length) {
+            for(var i = 0, len = stack.length; i < len; i++) {
+              clearSnackbar(stack[i], i);
+            }
           }
+
+          insertSnackbar();
+          snackbar.timeout = {
+            pop_out: $timeout(snackbarPopOut, POP_OUT_TIMEOUT),
+            remove: $timeout(removeSnackbar, REMOVE_TIMEOUT)
+          };
         }
 
-        insertSnackbar();
-        snackbar.timeout = {
-          pop_out: $timeout(snackbarPopOut, POP_OUT_TIMEOUT),
-          remove: $timeout(removeSnackbar, REMOVE_TIMEOUT)
-        };
-      }
+        function insertSnackbar() {
+          $animate.enter(snackbar, body, null, snackbarPopIn);
+          stack.push(snackbar);
+        }
 
-      function insertSnackbar() {
-        $animate.enter(snackbar, body, null, snackbarPopIn);
-        stack.push(snackbar);
-      }
+        function removeSnackbar() {
+          $animate.leave(snackbar);
+          stack.shift();
+        }
 
-      function removeSnackbar() {
-        $animate.leave(snackbar);
-        stack.shift();
-      }
+        function snackbarPopIn() {
+          $timeout(function snackbarPopInTimeout() {
+            snackbar.addClass(POP_UP);
+          }, 0);
+        }
 
-      function snackbarPopIn() {
-        $timeout(function snackbarPopInTimeout() {
-          snackbar.addClass(POP_UP);
-        }, 0);
-      }
+        function snackbarPopOut() {
+          snackbar
+            .addClass(POP_OUT)
+            .removeClass(POP_UP);
+        }
 
-      function snackbarPopOut() {
-        snackbar
-          .addClass(POP_OUT)
-          .removeClass(POP_UP);
-      }
+        function clearSnackbar(item, index) {
+          $timeout.cancel(item.timeout.pop_out);
+          $timeout.cancel(item.timeout.remove);
+          $animate.leave(item);
+          stack.splice(index, 1);
+        }
 
-      function clearSnackbar(item, index) {
-        $timeout.cancel(item.timeout.pop_out);
-        $timeout.cancel(item.timeout.remove);
-        $animate.leave(item);
-        stack.splice(index, 1);
-      }
+        function getStyles() {
+          return {
+            wrapper: {
+              'background-color': config['background-color'] || colors.notice,
+            },
+            message: {
+              'color': config.color || '#FFF'
+            }
+          };
+        }
 
-      function getStyles() {
-        return {
-          wrapper: {
-            'background-color': config['background-color'] || COLORS.DEFAULT,
-          },
-          message: {
-            'color': config.color || '#FFF'
-          }
-        };
-      }
+        function getPosition() {
+          var
+            position = POSITIONS[pos];
 
-      function getPosition() {
-        var
-          position = POSITIONS[pos];
-
-        return position ? POSITION_CLASSES[position] : POSITION_CLASSES.BOTTOM_LEFT;
+          return position ? POSITION_CLASSES[position] : POSITION_CLASSES.BOTTOM_LEFT;
+        }
       }
     }
   }
+
+
 
 })(angular);
